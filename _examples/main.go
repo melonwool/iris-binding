698f2abad3e7a9c8321b657cff780511fc96a81c
malong <malong@panda.tv>
2017-04-04 13:29:56 +0800

    use martini-binding for iris

---
+++ _examples/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/melonwool/iris-binding"
+	"gopkg.in/kataras/iris.v6"
+	"gopkg.in/kataras/iris.v6/adaptors/httprouter"
+)
+
+type User struct {
+	Name    string `form:"name" binding:"required"`
+	Age     int    `form:"age" binding:"required"`
+	Email   string `form:"email" binding:"required"`
+	Address string `form:"address" binding:"required"`
+}
+
+func main() {
+	port := flag.String("port", "8080", "http listen port")
+	flag.Parse()
+	app := iris.New()
+	app.Adapt(
+		iris.DevLogger(),
+		httprouter.New(),
+	)
+	app.Post("/user/add", UserAdd)
+
+	app.Listen(":" + *port)
+}
+
+func UserAdd(ctx *iris.Context) {
+	userInterface, errline, err := binding.Form(ctx, User{})
+	if errline > 0 {
+		//errmsg
+		fmt.Println("errors:", err)
+	}
+	user := userInterface.(User)
+	fmt.Printf("The Form value is %+v\n", user)
+}
